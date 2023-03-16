17a59ba668cfdb4505369e42291e3acf3df4cc9b
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-03-16 21:21:48 +0100

    Added https conntectivity

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 
 const app = express();
+
+const private_key = fs.readFileSync("/etc/letsencrypt/live/www.beryllium.dev/privkey.pem", "utf-8");
+
+const certificate = fs.readFileSync("/etc/letsencrypt/live/www.beryllium.dev/fullchain.pem", "utf-8");
+
+const ca = fs.readFileSync("/etc/letsencrypt/live/www.beryllium.dev/fullchain.pem", "utf-8");
+
+const credentials = {
+	key: private_key,
+	cert: certificate,
+	ca: ca
+};
 
 app.get("/", (req, res) => {
 	console.log("Test");
@@ -8,4 +22,6 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(443);
+const httpsServer = https.createServer(certificate, app);
+
+httpsServer.listen(443);
