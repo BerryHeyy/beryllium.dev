42abea482d3de5c264ebb783509a06b172df3148
BerryHeyy <berkaykaplanbk@hotmail.com>
2023-03-16 19:59:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	console.log("Test");
+
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(443);
